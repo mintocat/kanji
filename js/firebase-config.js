011c8b4bb8df31acc661401474fa687ca7745ee5
mintocat <nosfir4@gmail.com>
2026-03-14 17:53:04 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,7 @@
-// Firebase SDK の読み込み（CDN版）
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getDatabase, ref, set, onValue, push } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyB_awkpbzCSxtIGt4fKwcfyy2XmImkrv08",
     authDomain: "kanji-64bb5.firebaseapp.com",
     databaseURL: "https://kanji-64bb5-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -10,11 +9,11 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-
     storageBucket: "kanji-64bb5.firebasestorage.app",
     messagingSenderId: "583783676982",
     appId: "1:583783676982:web:f25957b9a38fb21c7a2c97"
-  };
+};
 
 // 初期化
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// 他のファイルから使えるようにエクスポート
-export { db };
+// 他のファイル（各ゲーム）で使えるようにエクスポート
+export { db, ref, set, onValue, push };
